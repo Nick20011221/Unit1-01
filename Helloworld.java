e3b1b40060bb115edeea08653a664864655b1eb8
Nick P-A <nicholas.pasco-arnone@mths.ca>
2020-02-11 18:02:29 +0000

    initial commit

---
+++ Helloworld.java
@@ -0,0 +1,10 @@
+/**made by Nick P-A.
+ * made on feb 11 2020
+ * displays hello world
+ * Class HelloWorld
+ */
+public class Helloworld {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
